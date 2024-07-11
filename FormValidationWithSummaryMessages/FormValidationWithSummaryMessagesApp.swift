1fcb1f7b9396695f55bff152dcc79e9811444125
DavidAdewale <adewaledavidola@gmail.com>
2024-07-11 18:22:05 +0100

    Initial Commit

---
+++ FormValidationWithSummaryMessages/FormValidationWithSummaryMessagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormValidationWithSummaryMessagesApp.swift
+//  FormValidationWithSummaryMessages
+//
+//  Created by David Adewale on 11/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct FormValidationWithSummaryMessagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
